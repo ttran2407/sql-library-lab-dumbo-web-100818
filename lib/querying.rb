19b16ac15c12d445cad768c2d43bdd351550c7d2
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 21:29:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,5 +45,6 @@ def select_character_names_and_number_of_books_they_are_in
     INNER JOIN character_books ON characters.id = character_books.character_id
     INNER JOIN books ON books.id = character_books.character_id 
     GROUP BY characters.name
+    ORDER BY book_count DESC
   "
 end
