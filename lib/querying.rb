c53560953da1217038e270acc6685922cba83f95
Tien Tran <tientran2407.vn@gmail.com>
2019-05-26 21:16:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
-  "SELECT characters.name, SUM(books.name) FROM characters
+  "SELECT characters.name, COUNT(books.name) FROM characters
     INNER JOIN character_books ON characters.id = character_books.character_id
     INNER JOIN books ON books.id = character_books.character_id 
   "
